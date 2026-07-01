1c7045fda731e6f0fea74b205f4dfd2ffe8005e5
Orynth-hub <orynth.dev@gmail.com>
2026-07-01 23:38:02 +0530

    Add Second Java solution to Medium

---
+++ Medium/003_Longest-Substring-Without-Repeating-Characters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // by sliding window
+        HashSet<Character> set = new HashSet<>();
+
+
+        int left = 0;
+        int max = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            char ch = s.charAt(right);
+
+            while(set.contains(ch)){
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(ch);
+
+            max = Math.max(max, right-left+1);
+        }
+        return max;
+    }
+}
